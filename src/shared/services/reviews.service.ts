a267641bb7299abe502d83ae5a1206301e595a95
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-05-01 21:26:08 +0300

    Add ReviewsService

---
+++ src/shared/services/reviews.service.ts
@@ -0,0 +1,51 @@
+import { ajax, AjaxResponse } from 'rxjs/ajax';
+import { Observable } from 'rxjs';
+import { apiEndpoint } from '../constants/apiEndpoint';
+import authService from './auth.service';
+import { switchMap } from 'rxjs/operators';
+
+export interface IReviewPostBody {
+  readonly rate: number;
+  readonly text: string;
+}
+
+export interface IReviewPostInput extends IReviewPostBody {
+  readonly product_id: string;
+}
+
+interface IReviewsService {
+  postReview(input: IReviewPostInput): Observable<AjaxResponse>;
+  getAllReviews(product_id: string): Observable<AjaxResponse>;
+}
+
+class ReviewsService implements IReviewsService {
+
+  postReview(input: IReviewPostInput) {
+    const body: IReviewPostBody = {
+      rate: input.rate,
+      text: input.text,
+    };
+
+    return authService.getToken().pipe(
+      switchMap((token) => ajax.post(
+        apiEndpoint + `reviews/${input.product_id}/`,
+        JSON.stringify(body),
+        {
+          'Content-Type': 'application/json; charset=utf-8',
+          'Authorization': `Token ${token}`,
+        },
+      )),
+    );
+  }
+
+  getAllReviews(product_id: string) {
+    return ajax.get(
+      apiEndpoint + `reviews/${product_id}/`,
+      {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+    );
+  }
+}
+
+export default new ReviewsService();
