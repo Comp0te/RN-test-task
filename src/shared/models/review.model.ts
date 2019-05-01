f83aef1b5d07eb9b790adc12b414939d58582346
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-05-01 22:48:44 +0300

    Add review model

---
+++ src/shared/models/review.model.ts
@@ -0,0 +1,26 @@
+export class ReviewModel {
+  id: number = 0;
+  product: number = 0;
+  userId: number = 0;
+  rate: number = 0;
+  text: string = '';
+
+  constructor(obj: ReviewResponse) {
+    for (const field in obj) {
+      if (typeof this[field] !== 'undefined') {
+        this[field] = obj[field];
+      }
+    }
+
+    if (obj.created_by && obj.created_by.id) {
+      this.userId = obj.created_by.id;
+    }
+  }
+}
+
+export interface ReviewResponse extends ReviewModel {
+  created_by: {
+    id: number;
+    username: string;
+  };
+}
