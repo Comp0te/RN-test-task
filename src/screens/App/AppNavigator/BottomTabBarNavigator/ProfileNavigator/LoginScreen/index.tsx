b2551607b6f44b57f41ba5f1f8b9842923dc01c7
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-05-04 19:27:19 +0300

    Add LoginScreen

---
+++ src/screens/App/AppNavigator/BottomTabBarNavigator/ProfileNavigator/LoginScreen/index.tsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import { compose, Dispatch } from 'redux';
+import { connect } from 'react-redux';
+import style from './style';
+
+import {
+  View, Text, TouchableOpacity, SafeAreaView,
+  KeyboardAvoidingView, ScrollView,
+} from 'react-native';
+import { Field, InjectedFormProps, reduxForm } from 'redux-form';
+import FieldInput from '../../../../../../shared/components/FieldInput';
+import CommonButton from '../../../../../../shared/components/CommonButton';
+
+import required from '../../../../../../shared/validators/required';
+import { passwordLogin } from '../../../../../../shared/validators/password';
+import { userNameLetters } from '../../../../../../shared/validators/userName';
+import { maxLengthUserName, minLengthUserName } from '../../../../../../shared/validators/lenght';
+
+import { AuthInput } from '../../../../../../shared/services/auth.service';
+import { RootState } from '../../../../../../redux/store';
+
+import navService from '../../../../../../../src/shared/services/nav.service';
+import * as loginAC from '../../../../../../redux/requests/requestsEntities/auth/login/AC';
+import { getIsLoginRequestLoading } from '../../../../../../redux/requests/selectors';
+
+type LoginFormData = AuthInput;
+
+interface StateProps {
+  isLoading: boolean;
+}
+
+const mapStateToProps = (state: RootState): StateProps => ({
+  isLoading: getIsLoginRequestLoading(state),
+});
+
+type Props = StateProps & InjectedFormProps<LoginFormData>;
+
+const LoginScreen: React.FC<Props> = (props) => {
+  const {handleSubmit, isLoading} = props;
+
+  const submitLogin = (values: LoginFormData, dispatch: Dispatch<loginAC.Actions>) => {
+    dispatch(loginAC.Actions.login(values));
+  };
+
+  const toRegistrationScreen = () => {
+    navService.navigate('RegistrationScreen');
+  };
+
+  return (
+    <SafeAreaView style={style.safeArea}>
+      <ScrollView contentContainerStyle={style.safeArea}>
+        <KeyboardAvoidingView
+          behavior='padding'
+          style={style.root}
+        >
+          <View style={style.fieldWrapper}>
+            <Field
+              name='username'
+              component={FieldInput}
+              keyboard='default'
+              placeholder='User name'
+              leftIconName='user'
+              maxLength={20}
+              validate={[
+                required,
+                userNameLetters,
+                maxLengthUserName,
+                minLengthUserName,
+              ]}
+            />
+          </View>
+          <View style={style.passwordWrapper}>
+            <Field
+              name='password'
+              component={FieldInput}
+              placeholder='Password'
+              leftIconName='lock'
+              validate={[required, passwordLogin]}
+              secureTextEntry={true}
+            />
+          </View>
+          <View style={style.signInWrapper}>
+            <CommonButton
+              title='SIGN IN'
+              loading={isLoading}
+              disabled={isLoading}
+              onPress={handleSubmit(submitLogin)}
+              iconName='sign-in'
+            />
+          </View>
+          <View style={style.singUpWrapper}>
+            <Text style={style.text}>
+              {'Do not have an account? '.toUpperCase()}
+            </Text>
+            <TouchableOpacity
+              onPress={toRegistrationScreen}
+              hitSlop={{
+                top: 10,
+                bottom: 10,
+                right: 0,
+                left: 0,
+              }}
+            >
+              <Text style={[style.text, style.textLink]}>SIGN UP</Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAvoidingView>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default compose<React.ComponentType>(
+  reduxForm<LoginFormData>({
+    form: 'login',
+  }),
+  connect(mapStateToProps),
+)(LoginScreen);
