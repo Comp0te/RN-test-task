88cf31b59a746a90bcea20c7cd6d036268e6deab
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-05-04 22:00:21 +0300

    Add RegistrationScreen

---
+++ src/screens/App/AppNavigator/BottomTabBarNavigator/ProfileNavigator/RegistrationScreen/index.tsx
@@ -0,0 +1,135 @@
+import React from 'react';
+import { compose, Dispatch } from 'redux';
+import { connect } from 'react-redux';
+import style from './style';
+
+import {
+  View, Text, TouchableOpacity, SafeAreaView,
+  KeyboardAvoidingView, ScrollView,
+} from 'react-native';
+import { Field, InjectedFormProps, reduxForm } from 'redux-form';
+import FieldInput from '../../../../../../shared/components/FieldInput';
+import CommonButton from '../../../../../../shared/components/CommonButton';
+
+import required from '../../../../../../shared/validators/required';
+import { passwordRegistration, confirmPassword } from '../../../../../../shared/validators/password';
+import { userNameLetters } from '../../../../../../shared/validators/userName';
+import { maxLengthUserName, minLengthUserName } from '../../../../../../shared/validators/lenght';
+
+import { AuthInput } from '../../../../../../shared/services/auth.service';
+import { RootState } from '../../../../../../redux/store';
+
+import navService from '../../../../../../../src/shared/services/nav.service';
+import * as registerAC from '../../../../../../redux/requests/requestsEntities/auth/register/AC';
+import { getIsRegisterRequestLoading } from '../../../../../../redux/requests/selectors';
+
+export interface RegisterFormData extends AuthInput {
+  confirmPassword: string;
+}
+
+interface StateProps {
+  isLoading: boolean;
+}
+
+const mapStateToProps = (state: RootState): StateProps => ({
+  isLoading: getIsRegisterRequestLoading(state),
+});
+
+type Props = StateProps & InjectedFormProps<RegisterFormData>;
+
+const LoginScreen: React.FC<Props> = (props) => {
+  const {handleSubmit, isLoading} = props;
+
+  const submitRegister = (values: RegisterFormData, dispatch: Dispatch<registerAC.Actions>) => {
+    const input: AuthInput = {
+      username: values.username,
+      password: values.password,
+    };
+
+    dispatch(registerAC.Actions.register(input));
+  };
+
+  const toLoginScreen = () => {
+    navService.navigate('LoginScreen');
+  };
+
+  return (
+    <SafeAreaView style={style.safeArea}>
+      <ScrollView contentContainerStyle={style.safeArea}>
+        <KeyboardAvoidingView
+          behavior='padding'
+          style={style.root}
+        >
+          <View style={style.fieldWrapper}>
+            <Field
+              name='username'
+              component={FieldInput}
+              keyboard='default'
+              placeholder='User name'
+              leftIconName='user'
+              maxLength={20}
+              validate={[
+                required,
+                userNameLetters,
+                maxLengthUserName,
+                minLengthUserName,
+              ]}
+            />
+          </View>
+          <View style={style.fieldWrapper}>
+            <Field
+              name='password'
+              component={FieldInput}
+              placeholder='Password'
+              leftIconName='lock'
+              validate={[required, passwordRegistration]}
+              secureTextEntry={true}
+            />
+          </View>
+          <View style={style.passwordWrapper}>
+            <Field
+              name='confirmPassword'
+              component={FieldInput}
+              placeholder='Confirm Password'
+              leftIconName='lock'
+              validate={[required, confirmPassword]}
+              secureTextEntry={true}
+            />
+          </View>
+          <View style={style.signInWrapper}>
+            <CommonButton
+              title='SIGN UP'
+              loading={isLoading}
+              disabled={isLoading}
+              onPress={handleSubmit(submitRegister)}
+              iconName='user-plus'
+            />
+          </View>
+          <View style={style.singUpWrapper}>
+            <Text style={style.text}>
+              {'Already have an account? '.toUpperCase()}
+            </Text>
+            <TouchableOpacity
+              onPress={toLoginScreen}
+              hitSlop={{
+                top: 10,
+                bottom: 10,
+                right: 0,
+                left: 0,
+              }}
+            >
+              <Text style={[style.text, style.textLink]}>SIGN IN</Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAvoidingView>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default compose<React.ComponentType>(
+  reduxForm<RegisterFormData>({
+    form: 'register',
+  }),
+  connect(mapStateToProps),
+)(LoginScreen);
---
+++ src/screens/App/AppNavigator/BottomTabBarNavigator/ProfileNavigator/RegistrationScreen/style.ts
@@ -0,0 +1,50 @@
+import { StyleSheet } from 'react-native';
+import { mainColor, greyColor } from '../../../../../../app.style';
+
+const style = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  root: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 95,
+    paddingBottom: 20,
+    paddingLeft: 40,
+    paddingRight: 40,
+    minHeight: 460,
+  },
+  fieldWrapper: {
+    width: '100%',
+  },
+  passwordWrapper: {
+    width: '100%',
+    marginBottom: 16,
+  },
+  signInWrapper: {
+    width: '100%',
+    height: 40,
+    marginBottom: 10,
+  },
+  singUpWrapper: {
+    position: 'absolute',
+    bottom: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  image: {
+    width: 160,
+    height: 33,
+  },
+  text: {
+    fontWeight: '400',
+    fontSize: 11,
+    color: greyColor,
+  },
+  textLink: {
+    color: mainColor,
+  },
+});
+
+export default style;
