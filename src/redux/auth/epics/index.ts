cc87e5f4b359c92eab545a4a4082fab34e4bc100
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-05-04 23:56:18 +0300

    Add auth redirection epics

--- src/redux/auth/epics/index.ts
+++ src/redux/auth/epics/index.ts
@@ -1,10 +1,11 @@
 import { Action } from 'redux';
 import { Observable } from 'rxjs';
 import { ofType } from 'redux-observable';
-import { ignoreElements, map, switchMap } from 'rxjs/operators';
+import { ignoreElements, map, switchMap, tap } from 'rxjs/operators';
 import * as fromActions from '../AC';
 import { requestsAC } from '../../requests/AC';
 import authService from '../../../shared/services/auth.service';
+import navService from '../../../shared/services/nav.service';
 
 export const setTokenEpic = (action$: Observable<Action>) => action$.pipe(
   ofType<Action, ReturnType<typeof requestsAC.login.Actions.loginSuccess> |
@@ -38,8 +39,28 @@ export const setIsAuthUserEpic = (action$: Observable<Action>) => action$.pipe(
   map(() => fromActions.Actions.setIsAuthUser(true)),
 );
 
+const redirectToProfileScreenEpic = (action$: Observable<Action>) => action$.pipe(
+  ofType<Action, ReturnType<typeof requestsAC.login.Actions.loginSuccess> |
+  ReturnType<typeof requestsAC.register.Actions.registerSuccess>>(
+    requestsAC.login.ActionTypes.LOGIN_REQUEST_SUCCESS,
+    requestsAC.register.ActionTypes.REGISTER_REQUEST_SUCCESS,
+  ),
+  tap(() => navService.navigate('ProfileScreen')),
+  ignoreElements(),
+);
+
+const redirectToLoginScreenEpic = (action$: Observable<Action>) => action$.pipe(
+  ofType<Action, fromActions.Actions>(
+    fromActions.ActionTypes.AUTH_LOGOUT,
+  ),
+  tap(() => navService.navigate('LoginScreen')),
+  ignoreElements(),
+);
+
 export const authEpics = [
   setTokenEpic,
   deleteTokenEpic,
   setIsAuthUserEpic,
+  redirectToProfileScreenEpic,
+  redirectToLoginScreenEpic,
 ];
