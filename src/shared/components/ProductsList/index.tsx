f39b1c5e3e62303cae9f8e556e6c15eb76310f71
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-05-05 21:21:29 +0300

    Add ProductList component

---
+++ src/shared/components/ProductsList/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import style from './style';
+
+import { View, FlatList, ListRenderItem } from 'react-native';
+import ProductItem from '../ProductItem';
+import CommonSearchBar from '../CommonSearchBar';
+
+export interface OwnProps {
+  isLoadingProducts: boolean;
+  productsIds: string[];
+  searchQuery: string;
+  onRefresh(): void;
+  onSearchProducts(query: string): void;
+}
+
+type Props = OwnProps;
+
+const ProductsList: React.FC<Props> = (props) => {
+  const {
+    productsIds, isLoadingProducts, searchQuery, onRefresh, onSearchProducts,
+  } = props;
+
+  const keyExtractor = (id: string) => `${id}`;
+
+  const renderItem: ListRenderItem<string> = ({item}) => {
+    return (
+      <ProductItem productId={item}/>
+    );
+  };
+
+  const getItemLayout = (data: string[] | null, index: number) => (
+    {length: 78, offset: 0.3, index}
+  );
+
+  const ListHeaderComponent = (
+    <CommonSearchBar
+      placeholder='Search by product name'
+      onChangeText={onSearchProducts}
+      value={searchQuery}
+    />
+  );
+
+  return (
+    <View style={style.root}>
+      <FlatList
+        data={productsIds}
+        keyExtractor={keyExtractor}
+        renderItem={renderItem}
+        onRefresh={onRefresh}
+        refreshing={isLoadingProducts}
+        initialNumToRender={10}
+        maxToRenderPerBatch={2}
+        getItemLayout={getItemLayout}
+        ListHeaderComponent={ListHeaderComponent}
+        contentContainerStyle={style.flatList}
+      />
+    </View>
+  );
+};
+
+export default React.memo(ProductsList);
