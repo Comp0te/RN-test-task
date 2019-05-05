54f01466fe98ead8cda98ea27dba834204c4d43c
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-05-06 01:27:48 +0300

    Add ProductDetailScreen to ProductNavigator

--- src/screens/App/AppNavigator/BottomTabBarNavigator/ProductsNavigator/index.tsx
+++ src/screens/App/AppNavigator/BottomTabBarNavigator/ProductsNavigator/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, NavigationScreenProps } from 'react-navigation';
+import { ProductDetailNavParams } from '../../../../../shared/components/ProductItem';
 
 import navService from '../../../../../shared/services/nav.service';
 
 import ProductsScreen from './ProductsScreen';
+import ProductDetailScreen from './ProductDetailScreen';
 
 export default createStackNavigator({
     ProductsScreen: {
       screen: ProductsScreen,
       navigationOptions: navService.navigationOptions('Products'),
+    },
+    ProductDetailScreen: {
+      screen: ProductDetailScreen,
+      navigationOptions: ({navigation}: NavigationScreenProps<ProductDetailNavParams>) => ({
+        ...navService.navigationOptions(''),
+        title: `${navigation.getParam('productTitle', '')}`,
+      }),
     },
   }
   , {
