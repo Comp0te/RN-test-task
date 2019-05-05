4a615154e22257c836fbf5c2bb8c282708c2c06a
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-05-05 21:22:38 +0300

    Add ProductsScreen

---
+++ src/screens/App/AppNavigator/BottomTabBarNavigator/ProductsNavigator/ProductsScreen/index.tsx
@@ -0,0 +1,78 @@
+import React, { useCallback, useEffect } from 'react';
+import { connect } from 'react-redux';
+import style from './style';
+
+import { SafeAreaView, View } from 'react-native';
+import ProductsList from '../../../../../../shared/components/ProductsList';
+
+import { Dispatch } from 'redux';
+import { RootState } from '../../../../../../redux/store';
+
+import { getIsGetAllProductsRequestLoading } from '../../../../../../redux/requests/requestsEntities/products/getAll/selectors';
+import { getFilteredProductsIds, getProductSearchQuery } from '../../../../../../redux/products/selectors';
+import { requestsAC } from '../../../../../../redux/requests/AC';
+import * as productAC from '../../../../../../redux/products/AC';
+
+interface StateProps {
+  isLoadingProducts: boolean;
+  productsIds: string[];
+  searchQuery: string;
+}
+
+interface DispatchProps {
+  getAllProducts(): void;
+  searchProducts(query: string): void;
+}
+
+const mapStateToProps = (state: RootState): StateProps => ({
+  isLoadingProducts: getIsGetAllProductsRequestLoading(state),
+  productsIds: getFilteredProductsIds(state),
+  searchQuery: getProductSearchQuery(state),
+});
+
+const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => (
+  {
+    getAllProducts: () => {
+      dispatch(requestsAC.getAllProducts.Actions.getAllProducts());
+    },
+    searchProducts: (query: string) => {
+      dispatch(productAC.Actions.setSearchQuery(query));
+    },
+  }
+);
+
+type Props = StateProps & DispatchProps;
+
+const ProductsScreen: React.FC<Props> = (props) => {
+  const {
+    productsIds, isLoadingProducts, getAllProducts, searchProducts, searchQuery,
+  } = props;
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
+  const onRefresh = useCallback(() => {
+    getAllProducts();
+  }, []);
+
+  const onSearchProducts = useCallback((query: string) => {
+    searchProducts(query);
+  }, []);
+
+  return (
+    <SafeAreaView style={style.safeArea}>
+      <View style={style.root}>
+        <ProductsList
+          productsIds={productsIds}
+          isLoadingProducts={isLoadingProducts}
+          onRefresh={onRefresh}
+          onSearchProducts={onSearchProducts}
+          searchQuery={searchQuery}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductsScreen);
---
+++ src/screens/App/AppNavigator/BottomTabBarNavigator/ProductsNavigator/ProductsScreen/style.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+import { whiteColor } from '../../../../../../app.style';
+
+const style = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  root: {
+    flex: 1,
+    paddingTop: 49,
+    paddingLeft: 20,
+    paddingRight: 20,
+    backgroundColor: whiteColor,
+  },
+});
+
+export default style;
